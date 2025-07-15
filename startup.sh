b95b41d5ca93fdf8ea178245048d77f325260b3b
shoichiro-suzuki <shoichiro_suzuki@musashi.co.jp>
2025-07-15 18:39:51 +0900

    20250715-1

--- startup.sh
+++ startup.sh
@@ -1,4 +1,1 @@
-python -m venv antenv
-source antenv/bin/activate
-pip install -r requirements.txt
-python -m reflex run --env prod --backend-port 8000
+python -m reflex run --env prod --backend-port 8000 --backend-host 0.0.0.0
